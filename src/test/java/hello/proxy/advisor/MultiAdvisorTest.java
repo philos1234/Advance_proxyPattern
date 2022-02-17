6beab2f32c0c4c3e9d579ecee6ecfc706b804d67
philos1234 <ohhyun1948@gmail.com>
2022-02-18 01:52:58 +0900

    스프링이 지원하는 프록시

---
+++ src/test/java/hello/proxy/advisor/MultiAdvisorTest.java
@@ -0,0 +1,84 @@
+package hello.proxy.advisor;
+
+import hello.proxy.common.advice.TimeAdvice;
+import hello.proxy.common.service.ServiceImpl;
+import hello.proxy.common.service.ServiceInterface;
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import org.springframework.aop.support.NameMatchMethodPointcut;
+
+public class MultiAdvisorTest {
+
+    /*
+      단점은, 어드바이저가 여러개라면 여러개의 프록시를 만들어야한다는 점이다.
+     */
+    @Test
+    @DisplayName("여러 프록시")
+    void multiAdvisorTest1(){
+        //client -> proxy2(advisor2) -> proxy1(advisor1) -> target
+
+        //프록시1 생성
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+
+        proxyFactory1.addAdvisor(advisor);
+        ServiceInterface proxy1 = (ServiceInterface) proxyFactory1.getProxy();
+
+        //프록시2 생성
+        ProxyFactory proxyFactory2 = new ProxyFactory(proxy1);
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE,new Advice2());
+        proxyFactory2.addAdvisor(advisor2);
+
+        ServiceInterface proxy2 = (ServiceInterface) proxyFactory2.getProxy();
+
+        proxy2.save();
+    }
+
+
+    @Slf4j
+    static class Advice1 implements MethodInterceptor{
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable{
+            log.info("advice1 호출");
+            return invocation.proceed();
+        }
+    }
+
+    @Slf4j
+    static class Advice2 implements MethodInterceptor{
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable{
+            log.info("advice2 호출");
+            return invocation.proceed();
+        }
+    }
+
+    @Test
+    @DisplayName("하나의 프록시, 여러 어드바이저")
+    void multiAdvisorTest2(){
+        //client -> proxy -> advisor2 -> advisor1 -> target
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new Advice1());
+        DefaultPointcutAdvisor advisor2 = new DefaultPointcutAdvisor(Pointcut.TRUE,new Advice2());
+
+        //프록시1 생성
+        ServiceInterface target = new ServiceImpl();
+        ProxyFactory proxyFactory1 = new ProxyFactory(target);
+
+        proxyFactory1.addAdvisor(advisor2); //먼저 들어감
+        proxyFactory1.addAdvisor(advisor);
+
+        ServiceInterface proxy = (ServiceInterface)proxyFactory1.getProxy();
+
+        proxy.save();
+    }
+
+
+}
